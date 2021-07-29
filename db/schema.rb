1b577136e4daf04aac47a2b809fc16ca20418a48
minako-y <m.tp2uk@gmail.com>
2021-07-29 14:13:41 +0000

    [修正]不要なモデルを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_16_142538) do
+ActiveRecord::Schema.define(version: 2021_07_16_142439) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -41,15 +41,6 @@ ActiveRecord::Schema.define(version: 2021_07_16_142538) do
     t.integer "followed_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "tag_maps", force: :cascade do |t|
-    t.integer "book_id"
-    t.integer "tag_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["book_id"], name: "index_tag_maps_on_book_id"
-    t.index ["tag_id"], name: "index_tag_maps_on_tag_id"
   end
 
   create_table "tags", force: :cascade do |t|
